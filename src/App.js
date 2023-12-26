4fa0e9b42c5f3c8f396342579ff0c4aa83f822c2
Juan Galvez <juanjosegalvez2014@gmail.com>
2023-12-26 10:37:58 -0500

    My First Version of the challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [bill, setBill] = useState(0);
+
+  const [tip, setTip] = useState(0);
+
+  function handleBill(payment) {
+    setBill(parseFloat(payment));
+  }
+
+  function handleTip(tip) {
+    setTip(parseFloat(bill * tip));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bill bill={bill} onHandleBill={handleBill} />
+      <Tips bill={bill} onHandleTip={handleTip} />
+      <Total bill={bill} tip={tip} />
     </div>
   );
 }
 
-export default App;
+function Bill({ bill, onHandleBill }) {
+  return (
+    <div>
+      <p>How much was the Bill?</p>
+      {/* This allow us to read the value in the input and use it.*/}
+      <input type="text" onChange={(e) => onHandleBill(e.target.value)} />
+    </div>
+  );
+}
+
+function Tips({ bill, onHandleTip }) {
+  return (
+    <div>
+      <p>How did you like the service?</p>
+      <form>
+        {/* Form with a dropdown menu for selecting tip percentage, triggering the handleTip function on change. */}
+        <select onChange={(e) => onHandleTip(Number(e.target.value))}>
+          <option value={0}>Dissastisfied (0%)</option>
+          <option value={0.05}>It was okay (5%)</option>
+          <option value={0.1}>it was good (10%)</option>
+          <option value={0.2}>Absolutely amazing (20%)</option>
+        </select>
+      </form>
+    </div>
+  );
+}
+
+function Total({ bill, tip }) {
+  return (
+    <div>
+      <p>{`Your total is $${bill + tip} (Your tip is ${parseInt(tip)})`}</p>
+      <button>Reset</button>
+    </div>
+  );
+}
